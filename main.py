8740cb1e23c8335217781f3318f1129157d0bdac
Bogdan Ene <b.ene@nelson.nl>
2021-05-10 17:02:17 +0200

    implementation with each file and their position -- curently throws error that data that is passed to job is more than 4 MB

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import numpy as np
 import io
 from util import copyfileobj
 import concurrent.futures
+import boto3
+from S3File import S3File
 
 # PARTITION_ROWS = 40000000  # 4 GB
-size_prefix = '100gb-1gb'
+size_prefix = '1000gb-1gb'
 input_prefix = f'{size_prefix}-input'
 output_prefix = f'{size_prefix}-output'
 intermediate_files_dir = f'{input_prefix}-intermediate-files'
-
 
 number_of_subcategories = 5
 
@@ -43,9 +44,8 @@ for i in range(nr_subcats):
             'max': (i + 1) * (95 // number_of_subcategories)
         }})
 
-
-number_of_lambda_sessions_phase_1 = 10
-number_of_lambda_sessions_phase_2 = 10
+number_of_lambda_sessions_phase_1 = 1
+number_of_lambda_sessions_phase_2 = 1
 
 
 def iterate_file(filename):
@@ -53,120 +53,96 @@ def iterate_file(filename):
         yield line
 
 
-def thread_upload(key_name, nr_files, value, bucket, key):
-    file_names = set()
-    file_name = key_name.split('/')[1]
-    new_file_name = f'{file_name}_{nr_files}'
-    intermediate_files_temp = ''
-    for k, v in subcategories.items():
-        if (
-                (not v['min'] and key <= v['max']) or
-                (not v['max'] and v['min'] < key) or
-                (v['min'] and v['max'] and v['min'] < key <= v['max'])
-        ):
-            intermediate_files_temp = f'{intermediate_files_dir}_{key}'
-            break
-    for second_key, second_value in value.items():
-        intermediate_file_dir_temp = f'{intermediate_files_temp}/{key}_{second_key}'
-        file_names.add(intermediate_file_dir_temp)
-        with open(f's3://{bucket}/{intermediate_file_dir_temp}/{new_file_name}',
-                  'wb') as intermediate_file:
-            file_bytes = b"".join(second_value)
-            intermediate_file.write(file_bytes)
-    return file_names
-
-# def thread_upload(key_name, nr_files, value, bucket, key):
-#     file_names = set()
-#     file_name = key_name.split('/')[1]
-#     new_file_name = f'{file_name}_{nr_files}'
-#     for second_key, second_value in value.items():
-#         file_names.add(f'{key}_{second_key}')
-#         with open(f's3://{bucket}/{intermediate_files_dir}/{key}_{second_key}/{new_file_name}',
-#                   'wb') as intermediate_file:
-#             file_bytes = b"".join(second_value)
-#             intermediate_file.write(file_bytes)
-#     return file_names
-
-
-def write_partition(key_name, chars, nr_files, bucket):
-    file_names = set()
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        futures_upload = {executor.submit(thread_upload, key_name, nr_files, value, bucket, key): (key, value) for key, value in chars.items()}
-        for future in concurrent.futures.as_completed(futures_upload):
-            file_names |= future.result()
-
-    return file_names
-
-
-def determine_partition(chars, line):
-    diff = line[1] - 32
-    if chars.get(line[0]):
-        for key, value in subcategories.items():
-            if (
-                    (not value['min'] and diff <= value['max']) or
-                    (not value['max'] and value['min'] < diff) or
-                    (value['min'] and value['max'] and value['min'] < diff <= value['max'])
-            ):
-                if chars[line[0]].get(key):
-                    chars[line[0]][key].append(line)
-                else:
-                    chars[line[0]].update({key: [line]})
-    else:
-        for key, value in subcategories.items():
-            if (
-                    (not value['min'] and diff <= value['max']) or
-                    (not value['max'] and value['min'] < diff) or
-                    (value['min'] and value['max'] and value['min'] < diff <= value['max'])
-            ):
-                chars.update({line[0]: {key: [line]}})
-    return chars
+def upload_sorted_initial_file(bucket, key_name, record_arr):
+    with open(f's3://{bucket}/{intermediate_files_dir}/{key_name}', 'wb') as sorted_file:
+        sorted_file.write(memoryview(record_arr))
 
 
 def determine_categories(key_name, storage):
-    nr_files = 1
-    count_rows = 0
-    chars = {}
-    unique_keys = set()
-    for line in iterate_file(f's3://{storage.bucket}/{key_name}'):
-        chars = determine_partition(chars, line)
-        count_rows += 1
-
-    # write remainings
-    if chars:
-        file_names = write_partition(key_name, chars, nr_files, storage.bucket)
-        unique_keys = unique_keys.union(file_names)
-    return unique_keys
-
-
-def sort_category(category_key_name, storage):
-    storage_client = Storage()
-    # keys = storage_client.list_keys(storage.bucket, f'{intermediate_files_dir}/{category_key_name}/')
-    keys = storage_client.list_keys(storage.bucket, f'{category_key_name}/')
-
     buf = io.BytesIO()
+    locations = {}
 
-    for key_name in keys:
-        with open(f's3://{storage.bucket}/{key_name}', 'rb',
-                  transport_params=dict(client=storage.get_client())) as myfile:
-            copyfileobj(myfile, buf)
+    with open(f's3://{storage.bucket}/{key_name}', 'rb',
+              transport_params=dict(client=storage.get_client())) as myfile:
+        copyfileobj(myfile, buf)
 
     category_buffer = buf.getbuffer()
     del buf
-    np_array = np.frombuffer(
-        category_buffer, dtype=np.dtype([('sorted', 'bytes', 1), ('key', 'bytes', 9), ('value', 'bytes', 90)])
-    )
+    record_arr = np.frombuffer(category_buffer, dtype=np.dtype([('key', 'bytes', 2), ('value', 'bytes', 98)]))
     del category_buffer
-    np_array = np.sort(np_array, order='key')
-    file_name = category_key_name.split('/')[1]
-    with open(f's3://{storage.bucket}/{output_prefix}/{file_name}', 'wb',
-              transport_params=dict(client=storage.get_client())) as sorted_file:
-        sorted_file.write(memoryview(np_array))
+    record_arr = np.sort(record_arr, order='key')
+    sorted_file_name = key_name.split('/')[1]
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+        executor.submit(upload_sorted_initial_file, storage.bucket, sorted_file_name, record_arr)
+
+    first_char = None
+    start_index = 0
+    current_file_number_per_first_char = 1
+
+    num_subcats = 5
+    diff = 95 / num_subcats
+    lower_margin = 0
+    upper_margin = diff
+    new_file_name = ''
+    nr_elements = 0
+    for nr_elements, rec in enumerate(record_arr):
+        if first_char is None:
+            first_char = rec[0][0]
+
+        new_file_name = f'{first_char}_{current_file_number_per_first_char}'
+        rec_diff = rec[0][1] - 32
+
+        if rec[0][0] != first_char or (rec_diff < lower_margin or rec_diff > upper_margin):
+            locations[new_file_name] = {
+                'start_index': start_index,
+                'end_index': nr_elements - 1,
+                'file_name': sorted_file_name
+            }
+            current_file_number_per_first_char += 1
+            lower_margin = upper_margin + 1
+            upper_margin = upper_margin + diff
+            start_index = nr_elements
+
+            if rec[0][0] != first_char:
+                current_file_number_per_first_char = 1
+                start_index = nr_elements
+                lower_margin = 0
+                upper_margin = diff
+                first_char = rec[0][0]
+
+    locations[new_file_name] = {
+        'start_index': start_index,
+        'end_index': nr_elements,
+        'file_name': sorted_file_name
+    }
+
+    return locations
+
+
+def sort_category(category_key_name, storage):
+    s3 = boto3.resource("s3")
+    for category_partition_name, files in category_key_name.items():
+        buf = io.BytesIO()
+        for file_name, indexes in files.items():
+            s3_object = s3.Object(bucket_name=storage.bucket, key=f'{intermediate_files_dir}/{file_name}')
+            s3file = S3File(s3_object, position=indexes['start_index']*100)
+            file_content = s3file.read(size=indexes['end_index']*100 - indexes['start_index']*100)
+            buf.write(file_content)
+
+        category_buffer = buf.getbuffer()
+        del buf
+        np_array = np.frombuffer(
+            category_buffer, dtype=np.dtype([('key', 'bytes', 10), ('value', 'bytes', 90)])
+        )
+        del category_buffer
+        np_array = np.sort(np_array, order='key')
+        with open(f's3://{storage.bucket}/{output_prefix}/{category_partition_name}', 'wb',
+                  transport_params=dict(client=storage.get_client())) as sorted_file:
+            sorted_file.write(memoryview(np_array))
 
 
 def sort():
-    max_parallelism = None
-    chars_set = set()
-
     with FunctionExecutor(runtime='bogdan/radix-sorting-container-100-mb-files') as fexec:
         bucket = fexec.config['lithops']['storage_bucket']
         storage_client = Storage()
@@ -176,50 +152,58 @@ def sort():
             'key_name': key
         } for key in storage_client.list_keys(bucket, input_prefix + '/')]
 
+        formatted = {}
+        determine_categories_result = []
         for i in range(number_of_lambda_sessions_phase_1):
             determine_categories_futures = fexec.map(determine_categories,
                                                      current_keys_list[
-                                                                   i * (len(current_keys_list) // number_of_lambda_sessions_phase_1):
-                                                                   (i+1) * (len(current_keys_list) // number_of_lambda_sessions_phase_1)])
-            determine_categories_result = fexec.get_result(determine_categories_futures)
-            for resulted_set in determine_categories_result:
-                chars_set |= resulted_set
+                                                     i * (len(current_keys_list) // number_of_lambda_sessions_phase_1):
+                                                     (i + 1) * (len(
+                                                         current_keys_list) // number_of_lambda_sessions_phase_1)])
+            determine_categories_result += fexec.get_result(determine_categories_futures)
 
-            print(f"============================ PHASE 1 ITERATION {i} ===============================")
-
-
-        # Process remainings
+        # PROCESS REMAININGS
         if len(current_keys_list) % number_of_lambda_sessions_phase_1 != 0:
             determine_categories_futures = fexec.map(determine_categories,
                                                      current_keys_list[
-                                                     (i + 1) * (len(current_keys_list) // number_of_lambda_sessions_phase_1):])
-            determine_categories_result = fexec.get_result(determine_categories_futures)
-            for resulted_set in determine_categories_result:
-                chars_set |= resulted_set
+                                                     (i + 1) * (len(
+                                                         current_keys_list) // number_of_lambda_sessions_phase_1):])
+            determine_categories_result += fexec.get_result(determine_categories_futures)
 
-        print(f"!!!!!!!!!!!!!!!!!!!!!!!!!! END PHASE 1 ITERATION {i} !!!!!!!!!!!!!!!!!!!!!!!!!!")
-
-        # PHASE 2
+        for file in determine_categories_result:
+            for category_partition_name, file_with_indexes in file.items():
+                if not formatted.get(category_partition_name):
+                    formatted[category_partition_name] = {
+                        file_with_indexes['file_name']: {
+                            'start_index': file_with_indexes['start_index'],
+                            'end_index': file_with_indexes['end_index']
+                        }
+                    }
+                else:
+                    formatted[category_partition_name].update({
+                        file_with_indexes['file_name']: {
+                            'start_index': file_with_indexes['start_index'],
+                            'end_index': file_with_indexes['end_index']
+                        }
+                    })
         fexec.config['serverless']['runtime_memory'] = 4800
-
-        chars_list = list(chars_set)
-
-        categories_keys_dict = [{
-            'category_key_name': key_name
-        } for key_name in chars_list]
-
-        # categories_keys_dict = [{'category_key_name': '2_2g-input'}]
-        for i in range(number_of_lambda_sessions_phase_2):
-            sort_categories_futures = fexec.map(sort_category, categories_keys_dict[
-                                                            i * (len(categories_keys_dict) // number_of_lambda_sessions_phase_2):
-                                                            (i+1) * (len(categories_keys_dict) // number_of_lambda_sessions_phase_2)])
+        formatted_list = [{'category_key_name': {key: value}} for key, value in formatted.items()]
+        print(formatted_list)
+        print("================== START PHASE 2 ======================")
+        nr_phases = 0
+        for nr_phases in range(number_of_lambda_sessions_phase_2):
+            sort_categories_futures = fexec.map(sort_category, formatted_list[
+                                                               nr_phases * (len(
+                                                                   formatted_list) // number_of_lambda_sessions_phase_2):
+                                                               (nr_phases + 1) * (len(
+                                                                   formatted_list) // number_of_lambda_sessions_phase_2)])
             sort_content_result = fexec.get_result(sort_categories_futures)
-            print(f"============================ PHASE 2 ITERATION {i} ===============================")
 
         # Process remainings
-        if len(categories_keys_dict) % number_of_lambda_sessions_phase_2 != 0:
-            sort_categories_futures = fexec.map(sort_category, categories_keys_dict[
-                                                            (i+1) * (len(categories_keys_dict) // number_of_lambda_sessions_phase_2):])
+        if len(formatted_list) % number_of_lambda_sessions_phase_2 != 0:
+            sort_categories_futures = fexec.map(sort_category, formatted_list[
+                                                               (nr_phases + 1) * (len(
+                                                                   formatted_list) // number_of_lambda_sessions_phase_2):])
             sort_content_result = fexec.get_result(sort_categories_futures)
 
         print("DONE")
